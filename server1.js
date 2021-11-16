c09fafa6970327802dda13773e87a391159f9c13
Caleb Rivera <calebwrivera@gmail.com>
2021-11-15 19:03:04 -0500

    server 1

---
+++ server1.js
@@ -0,0 +1,19 @@
+const https = require('https');
+
+const options = {
+    key: fs.readFileSync('private.key'),
+    cert: fs.readFileSync('certificate.crt')
+}
+
+app.use(express.static("express"));
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'))
+
+app.get('/main', checkNotAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname+'/express/main.html'));
+});
+
+const server = https.createServer(options, app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
