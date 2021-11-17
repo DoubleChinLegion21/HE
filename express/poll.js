2f47e055daaf6b15ae0d1708a92a2ecac5cbbe89
DoubleChinLegion21 <78381037+DoubleChinLegion21@users.noreply.github.com>
2021-11-16 20:18:34 -0500

    poll2

--- express/poll.js
+++ express/poll.js
@@ -1,3 +1,16 @@
+flavors = ["Vanilla","Matcha", "Chocolate", "Coconut", "Strawberry", "Banana", "Mango", "Coffee", "Pistachio", "Cookie Dough", "Oreo", "Peanut Butter"]
+
 function randomize(){
     console.log("hey")
+    shuffleArray(flavors)
+    console.log(flavors)
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
