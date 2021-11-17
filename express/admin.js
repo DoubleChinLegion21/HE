de19de61832fff2dfb456205c5d727f14fc0834d
Caleb Rivera <calebwrivera@gmail.com>
2021-11-17 16:49:20 -0500

    generate non boolean

--- express/admin.js
+++ express/admin.js
@@ -2,5 +2,5 @@ var ably = new Ably.Realtime('ug5isA._li3Cw:2GTzt_IvptxXrFnudQsXYzoJGtkgL59pSjjx
 var channel = ably.channels.get('HE');
 
 function generate(){
-    channel.publish('generate', true);
+    channel.publish('generate', "hey");
 }
