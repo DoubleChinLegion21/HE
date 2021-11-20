06ec4ccd6b1633e24e8ae77cf7587c912b1cec3f
Caleb Rivera <calebwrivera@gmail.com>
2021-11-20 10:31:44 -0500

    ciphertext generator which_db exclusion 2

--- server1.js
+++ server1.js
@@ -226,7 +226,7 @@ channel.subscribe('setmessage', function(message){
                     for (i in message.data){
                         ciphertext = ciphertext + String.fromCharCode(message.data[i].charCodeAt(0) ^ seed[i].charCodeAt(0));
                     }
-                    db.update({ name: sorted_docs[i].name }, { $set: { seed: seed, alt_ciphertext: alt_ciphertext} }, function (err, numReplaced) {});
+                    db.update({ name: sorted_docs[i].name }, { $set: { seed: seed, alt_ciphertext: ciphertext} }, function (err, numReplaced) {});
                 } else {
                     var new_total = total + sorted_docs[current_pos].number
                     // make seed and turn it into a string padded by zeros
