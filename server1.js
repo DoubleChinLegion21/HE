a21eb61b79183b5c258e5cefdfd28a534bc03df3
DoubleChinLegion21 <78381037+DoubleChinLegion21@users.noreply.github.com>
2021-11-21 16:36:18 -0500

    randomize

--- server1.js
+++ server1.js
@@ -138,6 +138,18 @@ app.post('/attempt_login', (req, res) => {
                     }
                     total += docs[i].number
                 }
+                if (found == false){
+                    seed = getRandomInt(0, total)
+                    total = -1
+                    for (i in docs){
+                        if (docs[i].number + total >= Number(seed)){
+                            console.log("found at", docs[i].name)
+                            nammm = docs[i].name
+                            break
+                        }
+                        total += docs[i].number
+                    }
+                }
                 to_send = [found, nammm, honeykey]
                 res.send(to_send)
             });
